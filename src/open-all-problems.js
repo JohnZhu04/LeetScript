20fc0e5328e83ac2a45ce114cfef4ed0f0d42af2
john@mac <20664734+JohnZhu04@users.noreply.github.com>
2022-08-22 16:41:36 +0900

    Add exclude and refactor

--- src/open-all-problems.js
+++ src/open-all-problems.js
@@ -1,33 +1,39 @@
 // ==UserScript==
-// @name        Open All Problems
+// @name        LeetCode Contest: Open All Problems
 // @namespace   JohnZhu04
 // @match       https://leetcode.com/contest/*-contest-*/
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      JohnZhu04
 // @license     MIT
 // @supportURL  https://github.com/JohnZhu04/LeetScript/issues
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=leetcode.com
+// @exclude     https://leetcode.com/contest/*/problems/*/
 // @description Add an "Open All Problems" button on the LeetCode Contest page. Note that Pop-up windows need to be allowed in the web browser.
 // ==/UserScript==
 
 const openAllProblems = () => {
-  const problems = document.querySelectorAll("ul.contest-question-list li a");
-  console.log(problems.length);
+  const problemsClass = "ul.contest-question-list li a";
+  const problems = document.querySelectorAll(problemsClass);
+  // console.log(problems.length);
   problems.forEach((problem) => {
     window.open(problem.href);
   });
 };
 
 const main = () => {
-  if (document.querySelector(".col-md-6") === null) {
+  const whichClass = ".col-md-6";
+  const buttonClass = "btn btn-default panel-hover";
+  const buttonText = "Open All Problems";
+  if (document.querySelector(whichClass) === null) {
+    // wait for the page to load
     window.setTimeout(main, 2000);
   }
   const button = document.createElement("button");
-  button.innerText = "Open All Problems";
+  button.innerText = buttonText;
   button.addEventListener("click", openAllProblems);
-  button.className = "btn btn-default panel-hover";
-  document.querySelector(".col-md-6").appendChild(button);
+  button.className = buttonClass;
+  document.querySelector(whichClass).appendChild(button);
 };
 
 main();
